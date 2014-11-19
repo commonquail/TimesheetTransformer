78176529b48d4d0a536e45e151e192ada42dba2e
Mikkel Kjeldsen <commonquail@gmail.com>
2014-11-19 19:12:17 +0100

    Unit test Rest calculation

--- source/timesheet/rest.d
+++ source/timesheet/rest.d
@@ -42,4 +42,94 @@ struct Rest
 
 private:
 
+unittest
+{
+    auto duration = 7.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 0,
+            "Should round duration of 7 minutes to 0 minutes.");
+}
+
+unittest
+{
+    auto duration = 8.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 15,
+            "Should round duration of 8 minutes to 15 minutes.");
+}
+
+unittest
+{
+    auto duration = 22.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 15,
+            "Should round duration of 22 minutes to 15 minutes.");
+}
+
+unittest
+{
+    auto duration = 23.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 30,
+            "Should round duration of 23 minutes to 30 minutes.");
+}
+
+unittest
+{
+    auto duration = 37.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 30,
+            "Should round duration of 37 minutes to 30 minutes.");
+}
+
+unittest
+{
+    auto duration = 38.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 45,
+            "Should round duration of 38 minutes to 45 minutes.");
+}
+
+unittest
+{
+    auto duration = 52.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 45,
+            "Should round duration of 52 minutes to 45 minutes.");
+}
+
+unittest
+{
+    auto duration = 53.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 0,
+            "Should round duration of 53 minutes to 0 minutes.");
+}
+
+unittest
+{
+    auto duration = 67.minutes;
+    Rest r;
+    r.update(duration);
+
+    assert(duration.minutes == 0,
+            "Should round duration of 67 minutes to 0 minutes.");
+}
+
 enum intervals = [60, 45, 30, 15, 0];
